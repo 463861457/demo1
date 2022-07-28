9cdfa45ccb65312033d51392d40a3130c5c1392e
lijb <463861457@qq.com>
2022-07-28 10:37:50 +0800

    拦截器

--- src/main/java/com/example/demo/config/WebConfig.java
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -6,12 +6,12 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration//表明这是配置类，并交给Spring管理
-public class WebConfig implements WebMvcConfigurer {
+public class WebConfig implements WebMvcConfigurer {   //WebMvcConfigurer    mvc的配置
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(loginInterceptor()).addPathPatterns("/**")//设置拦截路径；/**表示所有都拦截
-                .excludePathPatterns("/index/**");// 哪些路径不拦截（如果登录路径都拦截了用户还干什么啊），所以登录页面不能拦截
+                .excludePathPatterns("/index/**","/login");// 哪些路径不拦截（如果登录路径都拦截了用户还干什么啊），所以登录页面不能拦截
     }
 
     @Bean    //注入当前的拦截器
